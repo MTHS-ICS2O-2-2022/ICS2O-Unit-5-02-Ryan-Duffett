5bb51dab5e2d5f6158663664abf07c52abc66f5c
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-06-26 23:47:47 +0000

    Did the work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ryan Duffett
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+const randomNumberPositive = Math.floor(Math.random() * 6) + 1
+const randomNumberNegative = Math.floor(Math.random() * -6) + 1
+
+function myButtonClicked() {
+  buttonOnChecked = document.getElementById("positive-checked").checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your random number is: " + randomNumberPositive + "."
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your random number is: " + randomNumberNegative + "."
+  }
+}
